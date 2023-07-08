195c498e30eaa689fedefb4aa7de9446823fff12
Данило <119787050+Garikuhar@users.noreply.github.com>
2023-07-08 18:16:28 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+const imageElement = document.getElementById('imageElement');
+const images = ['Img/screenshot.png', 'Img/screenshot2.png']; // Додайте список URL-адрес зображень
+
+let currentIndex = 0;
+
+function changeImage() {
+  imageElement.classList.remove('fade-in-out'); // Видаляємо клас анімації
+
+  setTimeout(() => {
+    currentIndex = (currentIndex + 1) % images.length; // Оновлюємо індекс зображення
+    imageElement.src = images[currentIndex]; // Змінюємо src зображення
+
+    // Викликаємо reflow, щоб спрацювала анімація
+    void imageElement.offsetWidth;
+
+    imageElement.classList.add('fade-in-out'); // Додаємо клас анімації
+  }, 1000); // Затримка в 1 секунду
+}
+
+setInterval(changeImage, 3000);
+ // Змінюємо картинку кожні 3 секунди (3000 мілісекунд)
+ function copyText() {
+    var textToCopy = "138.2.156.44:31204"; // Замініть на потрібний текст для копіювання
+    navigator.clipboard.writeText(textToCopy)
+      .then(function() {
+        console.log("Текст скопійовано в буфер обміну");
+      })
+      .catch(function(error) {
+        console.error("Помилка копіювання тексту: ", error);
+      });
+  }
+  function downloadArchive() {
+    window.location.href = "pack.zip"; // Замініть на шлях до вашого архіву
+  }
+  function goToLink() {
+    window.location.href = "https://discord.gg/hSkWTjd9r"; // Замініть на ваше посилання на Discord
+  }
+  function playClickSound() {
+    var audio = document.getElementById("clickSound");
+    audio.play();
+  }    
